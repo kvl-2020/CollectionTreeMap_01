dacbc85009ae815246c1173f36b1153af5a28b57
Kozyaruk Vadim <kozyaruk@mail.ru>
2023-02-26 16:16:23 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,17 @@ public class Main {
         List<Person> people = new ArrayList<>();
 
         people.add(new Person("Иван", "Иванов", 20));
-        people.add(new Person("Хуан", "Карлос Родригос де Кастильо", 20));
         people.add(new Person("Педро", "Карлос Родригос де Кастильо", 40));
-        people.add(new Person("Махмуд", "Курлы Оглы Маглы", 15));
+        people.add(new Person("Хуан", "Карлос Родригос де Кастильо", 20));
+        people.add(new Person("Махмуд", "Курлы Оглы Маглы", 45));
 
         System.out.println(people);
-        System.out.println();
+
+        Collections.sort(people, new PersonStatusComparator(3));
+        System.out.println(people);
 
         Collections.sort(people, new PersonStatusComparator());
         System.out.println(people);
+
     }
 }
--- src/PersonStatusComparator.java
+++ src/PersonStatusComparator.java
@@ -3,11 +3,22 @@ package src;
 import java.util.Comparator;
 
 public class PersonStatusComparator implements Comparator<Person> {
+
+    private int importanteCount = Integer.MAX_VALUE;
+
+    public PersonStatusComparator() {
+    }
+
+    public PersonStatusComparator(int importanteCount) {
+        if (importanteCount > 0 ) {
+            this.importanteCount = importanteCount;
+        }
+    }
+
     @Override
     public int compare(Person o1, Person o2) {
-        //return Integer.compare(o1.surname.split(" ").length, o2.surname.split(" ").length);
-        int length1 = o1.surname.split(" ").length;
-        int length2 = o2.surname.split(" ").length;
+        int length1 = o1.surname.split(" ").length > importanteCount ? importanteCount : o1.surname.split(" ").length;
+        int length2 = o2.surname.split(" ").length > importanteCount ? importanteCount : o2.surname.split(" ").length;
         if (length1 > length2) {
             return 1;
         } else if (length1 < length2 ) {
@@ -16,4 +27,5 @@ public class PersonStatusComparator implements Comparator<Person> {
             return Integer.compare(o1.age, o2.age);
         }
     }
+
 }
